62d9dfe221145ed171256c09b756226d1f3e6fdb
Sheldon Bean <sheldon.bean@gmail.com>
2021-02-10 23:49:21 -0700

    catching up on in class examples

---
+++ 1610Project/Assets/Scripts/UserInput.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInput : MonoBehaviour
+{
+    public Rigidbody2D ballRigidbody2D;
+
+    public Vector2 xDirection,
+        yDirection;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            ballRigidbody2D.AddForce(xDirection, ForceMode2D.Force);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            ballRigidbody2D.AddForce(-xDirection, ForceMode2D.Force);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            ballRigidbody2D.AddForce(yDirection,ForceMode2D.Force);
+        }
+    }
+}
