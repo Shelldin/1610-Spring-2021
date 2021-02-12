f2afc880dffac98a2c3ea66ca860a0617404b888
Sheldon Bean <sheldon.bean@gmail.com>
2021-02-11 23:17:00 -0700

    More Class catch up

---
+++ 1610Project/Assets/Scripts/Counter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Counter : MonoBehaviour
+{
+    public int numberCount,
+        value = 10;
+    public Text counterText;
+
+    private void OnTriggerEnter2D()
+    {
+        numberCount += value;
+        counterText.text = numberCount.ToString();
+        gameObject.SetActive(false);
+    }
+    
+}
