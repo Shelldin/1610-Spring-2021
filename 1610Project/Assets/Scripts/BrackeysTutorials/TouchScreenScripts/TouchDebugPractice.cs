e7f556e0ff9979ba70a0063469661a39d19b48eb
Sheldon Bean <sheldon.bean@gmail.com>
2021-02-20 23:49:43 -0700

    struggling away with touch screen stuff not working properly

---
+++ 1610Project/Assets/Scripts/BrackeysTutorials/TouchScreenScripts/TouchDebugPractice.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchDebugPractice : MonoBehaviour
+{
+    
+    //from Brakeys Tutorial
+    void Update()
+    {
+        
+        for (int i = 0; i < Input.touchCount; i++)
+        {
+            //Touch touch = Input.GetTouch(i);
+            //Vector3 touchPos = touch.position;
+           // touchPos.z = 0f;
+            Vector3 touchPos = Camera.main.ScreenToWorldPoint(Input.touches[i].position);
+            Debug.DrawLine(Vector3.zero, touchPos, Color.green);
+        }
+    }
+}
