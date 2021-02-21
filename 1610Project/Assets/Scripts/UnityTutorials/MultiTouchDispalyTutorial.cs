c04e25f4065210d507812f3ef6cad68e5e12f423
Sheldon Bean <sheldon.bean@gmail.com>
2021-02-21 00:58:57 -0700

    Unity Touch Input Tutorials

---
+++ 1610Project/Assets/Scripts/UnityTutorials/MultiTouchDispalyTutorial.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MultiTouchDispalyTutorial : MonoBehaviour
+{
+   //from Unity Learn Tutorial
+   public Text multiTouchDisplay;
+
+   private int maxTapCount = 0;
+   private string multiTouchInfo;
+   private Touch touch;
+
+   void Update()
+   {
+       multiTouchInfo = string.Format("Max tap count: {0}\n", maxTapCount);
+
+       if (Input.touchCount > 0)
+       {
+           for (int i = 0; i < Input.touchCount; i++)
+           {
+               touch = Input.GetTouch(i);
+
+               multiTouchInfo +=
+                   string.Format("touch {0} - Position {1} - Tap Count: {2} - Finger ID: {3}\nRadious: {4} ({5}%)\n",
+                       i, touch.position, touch.tapCount, touch.fingerId, touch.radius, ((touch.radius / (touch.radius +
+                           touch.radiusVariance)) * 100f).ToString("F1"));
+               if (touch.tapCount > maxTapCount)
+               {
+                   maxTapCount = touch.tapCount;
+               }
+           }
+       }
+
+       multiTouchDisplay.text = multiTouchInfo;
+   }
+}
---
+++ 1610Project/Assets/Scripts/UnityTutorials/VirtualDPadTutorial.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VirtualDPadTutorial : MonoBehaviour
+{
+    //from Unity Learn Tutorial
+    public Text directionText;
+    private Touch touch;
+
+    private Vector2 touchStartPos,
+        touchEndPos;
+
+    private string direction;
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                touchStartPos = touch.position;
+            }
+            else if(touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Ended)
+            {
+                touchEndPos = touch.position;
+
+                float x = touchEndPos.x - touchStartPos.x;
+                float y = touchEndPos.y - touchStartPos.y;
+
+                if (Mathf.Abs(x) == 0 && Mathf.Abs(y) == 0)
+                {
+                    direction = "Tapped";
+                }
+                
+                else if (Mathf.Abs(x) > Mathf.Abs(y))
+                {
+                    direction = x > 0 ? "Right" : "Left";
+                }
+                else
+                {
+                    direction = y > 0 ? "Up" : "Down";
+                }
+            }
+        }
+
+        directionText.text = direction;
+    }
+}
