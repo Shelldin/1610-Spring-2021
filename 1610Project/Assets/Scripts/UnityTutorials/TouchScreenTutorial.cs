f3c48310991b9c9e45946e53a0d18ec1cb9cf2c7
Sheldon Bean <sheldon.bean@gmail.com>
2021-02-21 00:12:39 -0700

    Unity touch tutorial

---
+++ 1610Project/Assets/Scripts/UnityTutorials/TouchScreenTutorial.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TouchScreenTutorial : MonoBehaviour
+{
+    //from Unity Learn Tutorial
+    public Text phaseDisplayText;
+    private Touch touch;
+    private float touchEndTime;
+    private float timeDispaly = .5f;
+   
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Ended)
+            {
+                phaseDisplayText.text = touch.phase.ToString();
+                touchEndTime = Time.time;
+            }
+            else if(Time.time - touchEndTime > timeDispaly)
+            {
+                phaseDisplayText.text = touch.phase.ToString();
+                touchEndTime = Time.time;
+            }
+        }
+        else if (Time.time - touchEndTime>timeDispaly)
+        {
+            phaseDisplayText.text = "";
+        }
+    }
+}
