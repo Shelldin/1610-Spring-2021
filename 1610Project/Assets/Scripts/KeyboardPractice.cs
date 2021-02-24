b10e7b87298b50e953fafad0a0cc92954561959a
Sheldon Bean <sheldon.bean@gmail.com>
2021-02-24 00:23:08 -0700

    struggling against touchscreenkeyboard stuff

---
+++ 1610Project/Assets/Scripts/KeyboardPractice.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyboardPractice : MonoBehaviour
+{
+    public InputField inputField;
+    public string stringToEdit = "Hello World";
+    private TouchScreenKeyboard keyboard;
+
+    // from unity documentation
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
