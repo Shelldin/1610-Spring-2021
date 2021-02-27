c85f8564a1b9ed96cc914d4da7a58823a8217441
Sheldon Bean <sheldon.bean@gmail.com>
2021-02-27 00:19:43 -0700

    more in class scriptable Object stuff

---
+++ 1610Project/Assets/Scripts/BoostBehavior.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoostBehavior : MonoBehaviour
+{
+    public bool haveKey;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (haveKey)
+        {
+            
+        }
+    }
+}
---
+++ 1610Project/Assets/Scripts/GateKeeperBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateKeeperBehaviour : MonoBehaviour
+{
+    public BoolData keyObj;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        keyObj.value = true;
+    }
+}
