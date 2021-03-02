73948f7c7fbd588bb0442f1dc51c81d78030a0c2
Sheldon Bean <sheldon.bean@gmail.com>
2021-03-01 22:48:17 -0700

    For loops and arrays

---
+++ 1610Project/Assets/Scripts/LoopBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopBehaviour : MonoBehaviour
+{
+    public int number, total;
+    public string[] fruits;
+    public WeaponSO[] weapons;
+    
+    
+    private void Start()
+    {
+        for (int i = 0; i < number; i++)
+        {
+            total = i;
+            print(total);
+        }
+
+        foreach (var fruit in fruits)
+        {
+            print("I like to eat " + fruit);
+        }
+
+        foreach (var weapon in weapons)
+        {
+            print("You have my " + weapon.name + ", with a value of " + weapon.powerLevel);
+        }
+    }
+}
---
+++ 1610Project/Assets/Scripts/WeaponSO.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class WeaponSO : ScriptableObject
+{
+   public int powerLevel;
+
+}
