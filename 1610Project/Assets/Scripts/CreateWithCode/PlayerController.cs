a280d8f2a221a0755b38c9054c0131cd27a2ff93
Sheldon Bean <sheldon.bean@gmail.com>
2021-03-04 23:35:35 -0700

    actually create with code 1.4 (last time was 1.3)

--- 1610Project/Assets/Scripts/CreateWithCode/PlayerController.cs
+++ 1610Project/Assets/Scripts/CreateWithCode/PlayerController.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 20f;
-    void Start()
-    {
-        
-    }
-
+    public float turnSpeed;
+    private float horizontalInput;
+    private float forwardInput;
+    
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Translate(Vector3.right *Time.deltaTime * turnSpeed *horizontalInput);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
