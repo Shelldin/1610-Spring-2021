8ef641ada9c712358aba873f989461fad2ad3fb8
Sheldon Bean <sheldon.bean@gmail.com>
2021-03-15 23:17:42 -0600

    create with code plane project

---
+++ 1610Project/Assets/Challenge 1/Scripts/SpinPropellerBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinPropellerBehaviour : MonoBehaviour
+{
+    public GameObject propellerObj;
+    public Vector3 rotateVector3;
+
+    void FixedUpdate()
+    {
+        propellerObj.transform.Rotate(rotateVector3);
+    }
+}
