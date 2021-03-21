60189989ed94f5893af0ec0dad109405135425ac
Sheldon Bean <sheldon.bean@gmail.com>
2021-03-20 23:34:32 -0600

    2.4 Create with code

---
+++ 1610Project/Assets/Scripts/CreateWithCode/Lesson 2/DetectCollisionBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollisionBehaviour : MonoBehaviour
+{
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+}
--- 1610Project/Assets/Scripts/CreateWithCode/Lesson 2/SpawnManagerBehaviour.cs
+++ 1610Project/Assets/Scripts/CreateWithCode/Lesson 2/SpawnManagerBehaviour.cs
@@ -8,21 +8,26 @@ public class SpawnManagerBehaviour : MonoBehaviour
     public float spawnRangeX = 20,
         spawnPosZ = 20;
 
+    public float startDelay = 2f,
+        spawnInterval = 1.5f;
+
     private Vector3 spawnVector3;
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            spawnVector3.Set(Random.Range(-spawnRangeX,spawnRangeX),0,spawnPosZ);
+        
+    }
+
+    private void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        spawnVector3.Set(Random.Range(-spawnRangeX,spawnRangeX),0,spawnPosZ);
             
-            Instantiate(animalPrefabs[animalIndex], spawnVector3, animalPrefabs[animalIndex].transform.rotation);
-        }
+        Instantiate(animalPrefabs[animalIndex], spawnVector3, animalPrefabs[animalIndex].transform.rotation);   
     }
 }
