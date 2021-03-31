03afba563ef9069470f6b69567fedb74d570d481
Sheldon Bean <sheldon.bean@gmail.com>
2021-03-30 23:52:14 -0600

    Finished Brackeys Turn based combat Tutorial

--- 1610Project/Assets/Scripts/BrackeysTutorials/TurnBasedCombat/BattleSystem.cs
+++ 1610Project/Assets/Scripts/BrackeysTutorials/TurnBasedCombat/BattleSystem.cs
@@ -22,14 +22,21 @@ public class BattleSystem : MonoBehaviour
     public BattleHud enemyHud;
     public BattleHud playerHud;
 
+    public float seconds = 2f;
+    public float enemyTurnDelay = 1f;
+    private WaitForSeconds wfs;
+    private WaitForSeconds enemyWfs;
+
     public BattleState state;
     void Start()
     {
+        wfs = new WaitForSeconds(seconds);
+        enemyWfs = new WaitForSeconds(enemyTurnDelay);
         state = BattleState.START;
-        SetupBattle();
+        StartCoroutine(SetupBattle());
     }
 
-    private void SetupBattle()
+    IEnumerator SetupBattle()
     {
         Instantiate(playerPrefab, playerSpawnPoint);
         Instantiate(enemyPrefab, enemySpawnPoint);
@@ -38,5 +45,102 @@ public class BattleSystem : MonoBehaviour
         
         enemyHud.setHUD(enemyUnitSO);
         playerHud.setHUD(playerUnitSO);
+
+        yield return wfs;
+
+        state = BattleState.PLAYERTURN;
+        PlayerTurn();
+    }
+
+    IEnumerator PlayerAttack()
+    {
+        bool isDead = enemyUnitSO.TakeDamage(playerUnitSO.dmgStat);
+        
+        enemyHud.SetHP(enemyUnitSO.currentHp);
+        DialogueText.text = playerUnitSO.unitName + " deals " + playerUnitSO.dmgStat + "!";
+        
+        yield return wfs;
+
+        if (isDead)
+        {
+            state = BattleState.WON;
+            EndBattle();
+        }
+        else
+        {
+            state = BattleState.ENEMYTURN;
+            StartCoroutine(EnemyTurn());
+        }
+    }
+
+    IEnumerator EnemyTurn()
+    {
+        DialogueText.text = enemyUnitSO.unitName + " attacks!";
+
+        yield return enemyWfs;
+
+       bool isDead = playerUnitSO.TakeDamage(enemyUnitSO.dmgStat);
+        
+        playerHud.SetHP(playerUnitSO.currentHp);
+
+        yield return enemyWfs;
+
+        if (isDead)
+        {
+            state = BattleState.LOST;
+            EndBattle();
+        }
+        else
+        {
+            state = BattleState.PLAYERTURN;
+        }
+
+    }
+
+    void EndBattle()
+    {
+        if (state== BattleState.WON)
+        {
+            DialogueText.text = "You are victorious!";
+        }
+        else if (state == BattleState.LOST)
+        {
+            DialogueText.text = "You suffer a humiliating defeat.";
+        }
+    }
+    
+    void PlayerTurn()
+    {
+        DialogueText.text = "Choose an Action:";
+    }
+    
+
+    public void OnAttackButton()
+    {
+        if (state != BattleState.PLAYERTURN) return;
+
+            StartCoroutine(PlayerAttack());
+          
+    }
+
+    IEnumerator PlayerHeal()
+    {
+        playerUnitSO.Heal(5);
+        
+        playerHud.SetHP(playerUnitSO.currentHp);
+        DialogueText.text = playerUnitSO.unitName + " has healed for 5 hit points.";
+
+        yield return wfs;
+
+        state = BattleState.ENEMYTURN;
+        StartCoroutine(EnemyTurn());
+    }
+    
+    public void OnHealButton()
+    {
+        if (state != BattleState.PLAYERTURN) return;
+
+            StartCoroutine(PlayerHeal());
+          
     }
 }
--- 1610Project/Assets/Scripts/BrackeysTutorials/TurnBasedCombat/Unit.cs
+++ 1610Project/Assets/Scripts/BrackeysTutorials/TurnBasedCombat/Unit.cs
@@ -10,4 +10,28 @@ public class Unit : ScriptableObject
    public int dmgStat;
    public int maxHp;
    public int currentHp;
+
+   public bool TakeDamage(int dmg)
+   {
+      currentHp -= dmg;
+
+      if (currentHp <= 0)
+      {
+         return true;
+      }
+      else
+      {
+         return false;
+      }
+   }
+
+   public void Heal(int amount)
+   {
+      currentHp += amount;
+      if (currentHp > maxHp)
+      {
+         currentHp = maxHp;
+      }
+   }
+   
 }
