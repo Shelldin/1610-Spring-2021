1250d622ef50c6531a7b9e8e854bea32fe750a4f
Sheldon Bean <sheldon.bean@gmail.com>
2021-04-04 23:41:06 -0600

    createwithcode 3.1

---
+++ 1610Project/Assets/Scripts/CreateWithCode/Lesson 3/PlayerController3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController3 : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    
+    public float jumpForce = 10f;
+    public float gravityModifier = 1f;
+    public bool grounded;
+    
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+          playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+          grounded = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        grounded = true;
+    }
+}
