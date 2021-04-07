a198fefce8ed57f03374f905e17832f458ad3b16
Sheldon Bean <sheldon.bean@gmail.com>
2021-04-06 23:52:49 -0600

    createwithcode 4.1

---
+++ 1610Project/Assets/Scripts/CreateWithCode/Lesson 4/PlayerController4.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController4 : MonoBehaviour
+{
+    public float speed = 5f;
+
+    private Rigidbody playerRb;
+    public GameObject focalPoint;
+    
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("FocalPointGameObject");
+    }
+
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        
+        playerRb.AddForce(focalPoint.transform.forward * (forwardInput * speed * Time.deltaTime));
+    }
+}
