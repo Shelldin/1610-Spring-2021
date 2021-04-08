fe98084c01ae9b2ec1a2f449c9bc4dc2d7d76d56
Sheldon Bean <sheldon.bean@gmail.com>
2021-04-07 23:54:47 -0600

    createwithcode 4.2

---
+++ 1610Project/Assets/Scripts/CreateWithCode/Lesson 4/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public float speed = 3f;
+
+    private Rigidbody enemyRb;
+    private GameObject player;
+    void Start()
+    {
+        player = GameObject.Find("Sphere");
+        enemyRb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        enemyRb.AddForce((player.transform.position - transform.position).normalized * (speed * Time.deltaTime));
+    }
+}
