03b957909428816962dc4475ce382fc6b33f54cd
Sheldon Bean <sheldon.bean@gmail.com>
2021-04-09 19:14:30 -0600

    createwithcode 4.3

--- 1610Project/Assets/Scripts/CreateWithCode/Lesson 4/PlayerController4.cs
+++ 1610Project/Assets/Scripts/CreateWithCode/Lesson 4/PlayerController4.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,8 +7,13 @@ public class PlayerController4 : MonoBehaviour
 {
     public float speed = 5f;
 
+    private float powerUpStrength = 15;
+
     private Rigidbody playerRb;
     public GameObject focalPoint;
+    public GameObject powerUpIndicatorObj;
+
+    public bool hasPowerUp = false;
     
     void Start()
     {
@@ -20,5 +26,34 @@ public class PlayerController4 : MonoBehaviour
         float forwardInput = Input.GetAxis("Vertical");
         
         playerRb.AddForce(focalPoint.transform.forward * (forwardInput * speed * Time.deltaTime));
+        powerUpIndicatorObj.transform.position = transform.position + new Vector3(0, -.5f, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PowerUP"))
+        {
+            Destroy(other.gameObject);
+            hasPowerUp = true;
+            powerUpIndicatorObj.SetActive(true);
+            StartCoroutine(PowerUpCountdownRoutine());
+        }
+    }
+
+    IEnumerator PowerUpCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerUp = false;
+        powerUpIndicatorObj.SetActive(false);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Enemy") && hasPowerUp)
+        {
+            Rigidbody enemyRb = other.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = other.gameObject.transform.position - transform.position;
+            enemyRb.AddForce(awayFromPlayer * powerUpStrength, ForceMode.Impulse);
+        }
     }
 }
