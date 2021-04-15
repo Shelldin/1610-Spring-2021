62266e7035d5251ab19da16ed6b906b135ffa4c0
Sheldon Bean <sheldon.bean@gmail.com>
2021-04-14 23:49:57 -0600

    createwithcode 5.1

---
+++ 1610Project/Assets/Scripts/CreateWithCode/Lesson5/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+
+    private float spawnRate = 1f;
+    void Start()
+    {
+        StartCoroutine(SpawnTarget());
+    }
+
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+        
+    }
+}
---
+++ 1610Project/Assets/Scripts/CreateWithCode/Lesson5/Target.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Target : MonoBehaviour
+{
+    private Rigidbody targetRb;
+
+    private float minSpeed = 12f,
+        maxSpeed = 16f,
+        maxTorque = 10f,
+        xRange = 4f,
+        ySpawnPos = -2f;
+    void Start()
+    {
+        targetRb = GetComponent<Rigidbody>();
+        
+        targetRb.AddForce( RandomForce(), ForceMode.Impulse);
+        targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+
+        transform.position = RandomSpawnPos();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+       Destroy(gameObject); 
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+
+
+    private Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+
+    private float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+
+    private Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
+    }
+}
